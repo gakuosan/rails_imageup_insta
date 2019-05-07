1b91141df76a6e73420718b96a9f452ca0b284e5
Manabu Sato <gakuo19831201@gmail.com>
2019-05-07 22:35:06 +0000

    文字制限と空でメ ッセージを送れないようにする

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,6 +1,10 @@
 class Picture < ApplicationRecord
   mount_uploader:image,ImageUploader
   
+  validates :title, presence: true, length: { maximum: 15 }
+  validates :image, presence: true
+  validates :content, presence: true, length: { maximum: 30 }
+  
   belongs_to :user
   has_many :favorites, dependent: :destroy
   has_many :favorite_users,through: :favorites,source: :user
